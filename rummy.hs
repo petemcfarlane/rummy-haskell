c29eb5f8f8d9c7e0ebda68b6c7fdfc4729646333
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-11-26 16:56:43 +0000

    Add Rank and Card

--- rummy.hs
+++ rummy.hs
@@ -5,3 +5,26 @@ instance Show Suit where
     show Hearts = "♥"
     show Diamonds = "♦"
     show Clubs = "♣"
+
+data Rank =  Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord, Enum)
+
+instance Show Rank where
+    show Two = "2"
+    show Three = "3"
+    show Four = "4"
+    show Five = "5"
+    show Six = "6"
+    show Seven = "7"
+    show Eight = "8"
+    show Nine = "9"
+    show Ten = "10"
+    show Jack = "J"
+    show Queen = "Q"
+    show King = "K"
+    show Ace = "A"
+
+data Card = Card Rank Suit
+
+instance Show Card where
+    show (Card rank suit) = show rank ++ show suit
+
